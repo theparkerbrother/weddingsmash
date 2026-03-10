56fa3c7831b98a6a3b3d7c0d37ea756752dbd0fa
theparkerbrother <forrest273@gmail.com>
2026-03-10 13:11:03 -0600

    added staging indicator to frontend page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Welcome to Wedding Smash...Great things to come!</h1>
+      <h1>Welcome to Wedding Smash - Staging...Great things to come!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
